d8ab255b3cde10fb0201918929fed99fb2b31834
EriickChavez <10erickchavez10@gmail.com>
2024-07-30 13:31:09 -0600

    initial message

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -6,6 +6,9 @@ import ReportRoutes from "./Report";
 
 export default () => {
   const app = Router();
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server running' });
+  })
   AuthRoutes(app);
   ProductRoutes(app);
   CategoryRoutes(app);
